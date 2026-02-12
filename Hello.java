894a7a5ad672ecf5bc5a3d9ba4c5b32f46bac933
Mouath Nasri <mnasri@umich.edu>
2026-02-12 18:10:26 -0500

    Trigger auto Jenkins build

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello from the feature branch!");
+        System.out.println("Hello from the feature branch auto build test");
     }
 }
