352d828ec4ca8d0307fa7cd90f359cb8ef70dac9
Mouath Nasri <mnasri@umich.edu>
2026-02-12 16:37:34 -0500

    Updated on feature branch

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World - Updated on Main");
+        System.out.println("Hello from the feature branch!");
     }
 }
